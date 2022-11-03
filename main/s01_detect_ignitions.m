e52e142f8a0e2ab0395a3a7540f5d22317f59023
Adrià Descals <50245567+adriadescals@users.noreply.github.com>
2022-11-03 15:06:54 +0100

    Update s01_detect_ignitions.m

--- main/s01_detect_ignitions.m
+++ main/s01_detect_ignitions.m
@@ -1,34 +1,42 @@
+%% Detect the location of ignitions using FIRMS hotspot data
+% FIRMS data is not provided in the GitHub repository. The data can be
+% downloaded at https://firms.modaps.eosdis.nasa.gov/country/
+
 clear all; close all
 
 addpath('./auxiliary_code')
 
+%% Define sensor and time period
 sensor = 'modis'; %'viirs-snpp'; %'modis'
 yearsList = 2001:2020 %2012:2020 %2001:2020
 
+%% Define parameters
+lagDay = 30;
+mindDist = 10;
+
+%% Plot results
+plotResults1 = false;
+plotResults2 = false;
+
 for yy = yearsList
     
-% Arctic all
-pathData = ['C:\Users\adria\OneDrive - CREAF\PALM_PROJECT\_FIRE\_ARCTICfires_paper\R0_raw_backup\FIRMS_Russian_Federation\raw_data_' sensor '\'];
+%% read data
+pathData = ['..\large_datasets\FIRMS_Russian_Federation\raw_data_' sensor '\'];
 T = readtable([pathData sensor '_' num2str(yy) '_Russian_Federation.csv']);
 
 T(T.latitude<65,:) = [];
-T(T.longitude<72,:) = []; % avoid industrial areas in western siberia
+T(T.longitude<72,:) = []; % avoid industrial areas in Western Siberia
 
 [YY,MM,DD] = datevec(T.acq_date);
 doy = datenum(T.acq_date)-datenum(YY,1,1)+1;
 T = [array2table([YY,MM,DD,doy]) T];
 T.Properties.VariableNames = {'YY','MM','DD','doy',T.Properties.VariableNames{5:end}};
 
-lagDay = 30;
-mindDist = 10;
-
-%%
-
-
-
 lat = T.latitude;
 lon = T.longitude;
 doy = T.doy;
+
+%% Plot hotspots
 
 % figure, 
 % s = scatter3(lat,lon,doy,[],doy,'filled')
@@ -43,12 +51,7 @@ doy = T.doy;
 % colormap jet
 
 
-
-%%
-plotResults1 = false;
-plotResults2 = false;
-
-
+%% Initialize figures
 uniquedoy = unique(doy);
 status = zeros(length(doy),1);
 fireid = zeros(length(doy),1); 
@@ -65,13 +68,15 @@ if plotResults2
         s = plot(lon,lat,'.c')
 end
 
-for ii = 1:length(uniquedoy)
+%% do ignition detection
+for ii = 1:length(uniquedoy) % process from first to last DoY in FIRMS
    idoy = uniquedoy(ii);
    indBool = doy==idoy; 
    ind = find(indBool);
    lati = lat(ind);
    loni = lon(ind);
    
+%% STEP 1 - Apply temporal and spatial thresholds
    for jj = 1:length(lati)
        indSurr = (doy-idoy)<0 & (doy-idoy)>-lagDay & abs((lat-lati(jj)))<0.2 & abs((lon-loni(jj)))<0.2;
        if sum(indSurr) == 0
@@ -80,14 +85,14 @@ for ii = 1:length(uniquedoy)
             minDist2 = distance(lati(jj),loni(jj),lat(indSurr),lon(indSurr));
             dist2site = deg2km(minDist2);
             if min(dist2site)>mindDist
-                status(ind(jj)) = 1;
+                status(ind(jj)) = 1; % hotspot is an ignition
             else
-                status(ind(jj)) = 3;
+                status(ind(jj)) = 3; % hotspot is a propagation
             end
        end
    end
    
-   
+%% STEP 2 - Solve issue with rapid expansion of fire front
    for jj = 1:length(lati)
        if status(ind(jj)) == 1
            indSurr = (doy-idoy)==0 & abs((lat-lati(jj)))<0.2 & abs((lon-loni(jj)))<0.2;
@@ -97,31 +102,21 @@ for ii = 1:length(uniquedoy)
            
            if sum(dist2site<10)>1
                if max(statusSurr(dist2site<10)) == 3
-                   status(ind(jj)) = 3;
+                   status(ind(jj)) = 3; % hotspot is a propagation
                end
            end
            
        end
    end
    
-% T1 = clusterdata(X,3);
-   %%
-%    if idoy==175
-%        return
-%    end
-   
+
+%% STEP 3 - Select only one ignition from a cluster of ignition points  
    if sum((status==1 & doy==idoy))>1
        indnew = find(status==1 & doy==idoy);
        latnew = lat(indnew);
        lonnew = lon(indnew);
 
-%        C = clusterdata([latnew lonnew],15);
        [clustersCentroids,clustersGeoMedians,clustersXY,C] = clusterXYpoints([latnew lonnew],0.1);
-       
-%        C = zeros(length(indnew),1);
-%        for hh1 = 1:length(indnew) 
-%            
-%        end
        
 %         figure('units','normalized','outerposition',[0 0 1 1]), hold on
 %             plot(lon,lat,'.k')
@@ -129,7 +124,7 @@ for ii = 1:length(uniquedoy)
 %             colormap jet
 
         for hh1 = 1:length(indnew)
-            fireid(indnew(hh1)) = (yy-1990)*100000+idoy*1000+C(hh1);
+            fireid(indnew(hh1)) = (yy-1990)*1000000+idoy*1000+C(hh1); % assign an ID
         end
         
         for icluster = 1:length(C)
@@ -140,7 +135,7 @@ for ii = 1:length(uniquedoy)
         end
    elseif sum((status==1 & doy==idoy))==1
         indnew = find(status==1 & doy==idoy);
-        fireid(indnew) = (yy-1990)*100000+idoy*1000+1;
+        fireid(indnew) = (yy-1990)*1000000+idoy*1000+1;
    end
 
    indProp = (not(status==0) & not(status==1) & doy==idoy);
@@ -162,9 +157,8 @@ for ii = 1:length(uniquedoy)
            end
        end
    end
-   
-   
-   
+
+%% Plot results
 if plotResults2
     scatter(lon(fireid>0),lat(fireid>0),[],fireid(fireid>0),'filled')
     plot(lon(status==1),lat(status==1),'xr','MarkerSize',20)
@@ -176,7 +170,6 @@ if plotResults2
     pause(0.5)
 end
    
-   %%
 if plotResults1
         plot(lon(status==3 & doy==idoy),lat(status==3 & doy==idoy),'*c','MarkerSize',6)
         plot(lon(status==2),lat(status==2),'*m','MarkerSize',6)
@@ -191,15 +184,14 @@ display([num2str(ii) '/' num2str(length(uniquedoy)) ' DONE'])
 
 end
 
-%% Save
-
+%% Save results for each year
 OUT = table(doy,lat,lon,status,fireid);
 writetable(OUT,['./DATA/ignitions/' sensor '_' num2str(yy) '_Russian_Federation_ignitions_v1-1.csv'])
 
 end
 
 
-%% merge results
+%% Merge results for each year into one single csv file
 if strcmp(sensor,'modis')
     OUTall = [];
     for yy = 2001:2020
@@ -210,7 +202,6 @@ if strcmp(sensor,'modis')
     writetable(OUTall,['./DATA/modis_2001-2020_Russian_Federation_ignitions_v1-1.csv'])
 end
 
-%% merge results
 if strcmp(sensor,'viirs-snpp')
     OUTall = [];
     for yy = 2012:2020
@@ -224,23 +215,3 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
