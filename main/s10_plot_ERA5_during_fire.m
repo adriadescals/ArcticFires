582ca6e8d7453ac5746d5a4f80cc72cab3911a49
Adrià Descals <50245567+adriadescals@users.noreply.github.com>
2022-11-03 15:11:08 +0100

    Update s10_plot_ERA5_during_fire.m

--- main/s10_plot_ERA5_during_fire.m
+++ main/s10_plot_ERA5_during_fire.m
@@ -1,13 +1,16 @@
+% Generate supplementary figure 9. Fig. S9. Local weather conditions in the
+% fires detected in the Siberian Arctic during 2001-2020.
+
 clear all; close all;
 
-load('./DATA/STATS_ERA5hourly_ignitions','META','OUT')
+load('./DATA/STATS_ERA5hourly_ignitions','META','OUT') % load hourly data
 
-OUT(sum(isnan(OUT),2)>0,:) = [];
-Ws = sqrt(OUT(:,4).^2 + OUT(:,5).^2);
-Wd = mod(180+180/pi.*atan2(OUT(:,4),OUT(:,5)),360);
-OUT(:,3) = OUT(:,3).*1000;
+OUT(sum(isnan(OUT),2)>0,:) = []; % filter out raws with no data
+Ws = sqrt(OUT(:,4).^2 + OUT(:,5).^2); % calculate wind speed
+Wd = mod(180+180/pi.*atan2(OUT(:,4),OUT(:,5)),360); % calculate wind direction
+OUT(:,3) = OUT(:,3).*1000; % convert precipitation to mm
 
-%% MAIN PLOT
+%% PLOT
 
 figure, 
 subplot(2,2,1)
@@ -34,115 +37,7 @@ set(gca,'XLim',[0 360],'XTick',0:90:360)
 set(gca, 'YGrid', 'on', 'XGrid', 'on')
 
 % SAVE IMAGE as it appear on screen
-set(gcf, 'PaperPositionMode', 'auto')
-saveas(gcf,['./figures/era5_during_fire_event.svg'])
-
-
-%% OTHER PLOTS
-
-th = 4000
-ind = OUT(:,1)>th;
-figure, 
-subplot(2,2,1)
-hist(OUT(ind,2),50)
-xlabel('Maximum air temperature (°C)'), ylabel('Frequency')
-
-subplot(2,2,2)
-hist(OUT(ind,3),50)
-xlabel('Total precipitation 10-d before ignition (mm)'), ylabel('Frequency')
-
-subplot(2,2,3)
-hist(Ws(ind),50)
-xlabel('Wind speed (m^2/s)'), ylabel('Frequency')
-
-subplot(2,2,4)
-hist(Wd(ind),50)
-xlabel('Wind direction (0 North - 90 East - 180 South - 270 West)'), ylabel('Frequency')
-
-set(gca,'XLim',[0 360],'XTick',0:90:360)
-set(gca, 'YGrid', 'off', 'XGrid', 'on')
-title('')
-
-sgtitle(['Fire events >' num2str(th) ' ha'])
-
-
-
-ind = OUT(:,1)<th;
-figure, 
-subplot(2,2,1)
-hist(OUT(ind,2),50)
-xlabel('Maximum air temperature (°C)'), ylabel('Frequency')
-
-subplot(2,2,2)
-hist(OUT(ind,3),50)
-xlabel('Total precipitation 10-d before ignition (mm)'), ylabel('Frequency')
-
-subplot(2,2,3)
-hist(Ws(ind),50)
-xlabel('Wind speed (m^2/s)'), ylabel('Frequency')
-
-subplot(2,2,4)
-hist(Wd(ind),50)
-xlabel('Wind direction (0 North - 90 East - 180 South - 270 West)'), ylabel('Frequency')
- 
-set(gca,'XLim',[0 360],'XTick',0:90:360)
-set(gca, 'YGrid', 'off', 'XGrid', 'on')
-
-sgtitle(['Fire events <' num2str(th) ' ha'])
-
-
-%% STATS
-
-th = 4000; %prctile(OUT(:,1),90)
-ind = OUT(:,1)>th;
-
-% Ratios
-sum(ind)./length(ind)
-sum(OUT(ind,1))./sum(OUT(:,1))
-
-% TEMPERATURE
-
-mean(OUT(ind,2))
-std(OUT(ind,2))
-
-mean(OUT(not(ind),2))
-std(OUT(not(ind),2))
-
-ttest2(OUT(ind,2),OUT(not(ind),2))
-
-% Rainfall
-mean(OUT(ind,3))
-std(OUT(ind,3))
-
-mean(OUT(not(ind),3))
-std(OUT(not(ind),3))
-
-ttest2(OUT(ind,3),OUT(not(ind),3))
-
-% Wind speed
-mean(Ws(ind))
-std(Ws(ind))
-
-mean(Ws(not(ind)))
-std(Ws(not(ind)))
-
-figure, hold on
-histogram(Ws(ind))
-histogram(Ws(not(ind)))
-
-ttest2(Ws(ind),Ws(not(ind)))
-
-% Wind direction
-mean(Wd(ind))
-std(Wd(ind))
-
-mean(Wd(not(ind)))
-std(Wd(not(ind)))
-
-figure, hold on
-histogram(Wd(ind))
-histogram(Wd(not(ind)))
-
-ttest2(Wd(ind),Wd(not(ind)))
+% set(gcf, 'PaperPositionMode', 'auto')
+% saveas(gcf,['./figures/era5_during_fire_event.svg'])
 
 
