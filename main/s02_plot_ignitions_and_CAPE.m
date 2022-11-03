de4743bb53a7323d69c26e8557e1060bef9f5359
Adrià Descals <50245567+adriadescals@users.noreply.github.com>
2022-11-03 15:07:33 +0100

    Update s02_plot_ignitions_and_CAPE.m

--- main/s02_plot_ignitions_and_CAPE.m
+++ main/s02_plot_ignitions_and_CAPE.m
@@ -1,29 +1,28 @@
+%% Generate supplementary figure 6. Histograms of ignition and active fire detections, 
+% and the convective available potential energy in the Siberian Arctic.
+
 clear all; close all;
 
-%%
+%% Load hourly CAPE data averaged over the Siberian Arctic
+load('./DATA/Arctic_CAPE_hourly_2001-2020','meanCAPE','doyCAPE')
 
-load('./DATA/Arctic_CAPE_RF_hourly_2001-2020','meanCAPE','meanRF','doyCAPE')
+%% Load FIRMS ignitions
+IGN = readtable(['./DATA/modis_2001-2020_Russian_Federation_ignitions_v1-1.csv']);
+indOk = IGN.lat>66.5 & IGN.lon>85; % Define region of interest
+IGN(not(indOk),:) = [];
 
-%%
-T = readtable(['./DATA/BA_Arctic_FIRMS_perimeters_v1.csv']);
-T2 = readtable(['./DATA/modis_2001-2020_Russian_Federation_ignitions_v1-1.csv']);
-
-indOk = T2.lat>66.5 & T2.lon>85;
-T2(not(indOk),:) = [];
-
-BA = readtable('G:\My Drive\_ARCTIC_FIRES\_CODE\R1\CLEANED\MAIN\DATA/BA_Arctic_allSatellites_v1-4.csv');
-BAdata = table2array(BA(:,2:7))'./1000000;
+%% Load annual burned area
+BA = readtable('./DATA/BA_Arctic_allSatellites_v1.csv');
+BAdata = table2array(BA(:,2:7))'./1000000; % Convert to Mha
 products = BA.Properties.VariableNames;
 products = {products{2:end}};
 BAdata = nanmedian(BAdata);
 BAdata = BAdata(end-19:end);
 
-for iplot = 1:20 %[1 13 18 19 20]
+%% Generate figure
+for iplot = 1:20 % years from 2001 to 2020
 
-doys2020 = T.minDoy(T.year==iplot+2000);
-doysUnique = unique(doys2020);
-
-doys2020frims = T2.doy(T2.year==iplot+2000);
+doys2020frims = IGN.doy(IGN.year==iplot+2000);
 doysUniquefirms = unique(doys2020frims);
 FIRMSacc = [];
 for ii = 1:length(doysUniquefirms)
@@ -34,10 +33,10 @@ FIRMSacc = [FIRMSacc 1];
 doysUniquefirms = [doysUniquefirms; 244];
 
 figure('units','normalized','outerposition',[0.2 0.2 0.16 0.35]), hold on
-    hist(T.minDoy(T.year==iplot+2000),365)
+    hist(IGN.doy(IGN.status==1 & IGN.year==iplot+2000),365,'FaceColor','black','EdgeColor','black')
     plot(121:0.0417:244,meanCAPE(3:end,iplot)/10,'.r')
-    plot(doysUniquefirms,FIRMSacc*60,'c','LineWidth',2.5)
-    ylim([0 60])
+    plot(doysUniquefirms,FIRMSacc*40,'c','LineWidth',2.5)
+    ylim([0 40])
     xlim([121 244 ])
     box on
     xlabel('DoY')
@@ -47,10 +46,9 @@ figure('units','normalized','outerposition',[0.2 0.2 0.16 0.35]), hold on
 set(gca,'XTickLabelRotation',45)
 set(gca,'FontName','Arial','FontSize',12);
 
-%% SAVE IMAGE as it appear on screen
-set(gcf, 'PaperPositionMode', 'auto')
-saveas(gcf,['./figures/CAPE_ignitions_yy' num2str(iplot+2000) '.png'])
-
+% % SAVE IMAGE as it appear on screen
+% set(gcf, 'PaperPositionMode', 'auto')
+% saveas(gcf,['./figures/CAPE_ignitions_yy' num2str(iplot+2000) '.png'])
 
 end
     
