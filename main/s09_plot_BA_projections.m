b52748a23d0b04cfa456c4b903467dfa0a6bf4cc
Adrià Descals <50245567+adriadescals@users.noreply.github.com>
2022-11-03 15:10:45 +0100

    Update s09_plot_BA_projections.m

--- main/s09_plot_BA_projections.m
+++ main/s09_plot_BA_projections.m
@@ -1,37 +1,37 @@
+% Generate figure 6. Projected temperatures, annual burned areas (BA), and 
+% emissions from fire in the Siberian Arctic.
+
 clear all; close all;
 
 addpath('./auxiliary_code/')
 
-load('./DATA/TAS_summer_hadgem2_rcp45_rcp85.mat')
-T = readtable('./DATA/ARCTIC_TRENDS_Ta_LST_LSTera5_Rf_Vcomp_Ucomp_Vpd_Cwd_SoS_EoS_NDVI_v3-3.csv');
+load('./DATA/TAS_summer_hadgem2_rcp45_rcp85.mat') % load CMIP5 summer air temperature averaged over siberia
+T = readtable('./DATA/ARCTIC_TRENDS_Ta_LST_LSTera5_Rf_Vcomp_Ucomp_Vpd_Cwd_SoS_EoS_NDVI_v1.csv'); % load factors of fire
 
-%%
+%% Linear fit for the ERA5-Land air temperature
 ivar = 2;
 Y = T.Ta;
 X = T.year;
  
-funcType = {'poly1','poly2','exp1'};
-[f gof] = fit(X, Y, 'poly1' );
+[f gof] = fit(X, Y, 'poly1');
 
 lm = fitlm(X,Y);
-pvalue = lm.Coefficients.pValue(2)
-
+pvalue = lm.Coefficients.pValue(2);
 
 R2 = gof.rsquare; 
 [R pvalR] = corrcoef(f(X),Y);
 R2 = R(1,2)^2;
 pvalR = pvalR(1,2);
 
+%% Plot Fig. 6
 
-%%
 figure('units','normalized','outerposition',[0 0.2 0.33 0.6]), hold on
 
-subplot(2,2,1), hold on
+subplot(4,4,[1 2 5 6]), hold on
     plot(1940:2110,repmat(Y(end),length(1940:2110)),'k')
     plot(1940:2110,repmat(9.929,length(1940:2110)),'k')
     plot(X, Y, '.','MarkerSize',12,'Color',[0.5 0.5 0.5])
     plot(1950:2004,tsRCP45_summer(1:55),'.k')
-%     plot(T.yy,T.Ta,'og')
     plot(2005:2100,tsRCP45_summer(56:end),'.b')
     plot(2005:2100,tsRCP85_summer(56:end),'.r')
     plot(1980:2100,f(1980:2100),'--','Color',[0.5 0.5 0.5])
@@ -47,58 +47,26 @@ subplot(2,2,1), hold on
     set(gca,'FontName','Arial','FontSize',10);
 
 
-% figure('units','normalized','outerposition',[0 0.2 0.6 0.65]), hold on
-subplot(2,2,2), hold on
-
-    % PEAT
-    th1 = 0.5; th2 = 1;
-    
-    RCP45 = readtable('./DATA/RCP45_peat_simulations_results_v1.csv');
+    % load burned area projections
+    RCP45 = readtable('./DATA/RCP45_all_simulations_results_v1.csv'); % Projections RCP45 all Siberian Arctic
     TREND_BA = table2array(RCP45(:,1:2));
-    ba_rcp45 = TREND_BA(:,1)+0.11*TREND_BA(:,1);
-    RCP85 = readtable('./DATA/RCP85_peat_simulations_results_v1.csv');
+    ba_rcp45all = TREND_BA(:,1)+0.11*TREND_BA(:,1);
+    RCP85 = readtable('./DATA/RCP85_all_simulations_results_v1.csv'); % Projections RCP85 all Siberian Arctic
     TREND_BA = table2array(RCP85(:,1:2));
-    ba_rcp85 = TREND_BA(:,1)+0.11*TREND_BA(:,1);
+    ba_rcp85all = TREND_BA(:,1)+0.11*TREND_BA(:,1);
 
-    baClass_rcp45 = ones(length(ba_rcp45),1)*0.5;
-    baClass_rcp45(ba_rcp45<th1) = 0;
-    baClass_rcp45(ba_rcp45>th2) = 1;
-
-    baClass_rcp85 = ones(length(ba_rcp45),1)*0.5;
-    baClass_rcp85(ba_rcp85<th1) = 0;
-    baClass_rcp85(ba_rcp85>th2) = 1;
-    
-    plot(1950:2100,baClass_rcp45-0.04,'.b')
-    plot(1950:2100,baClass_rcp85+0.04,'.r')
-    
-    
-    % ALL
-    th1 = 1; th2 = 2.5;
-    
-    RCP45 = readtable('./DATA/RCP45_all_simulations_results_v1.csv');
+    RCP45 = readtable('./DATA/RCP45_peat_simulations_results_v1.csv'); % Projections RCP45 only peatlands
     TREND_BA = table2array(RCP45(:,1:2));
-    ba_rcp45 = TREND_BA(:,1)+0.11*TREND_BA(:,1);
-    RCP85 = readtable('./DATA/RCP85_all_simulations_results_v1.csv');
+    ba_rcp45peat = TREND_BA(:,1)+0.11*TREND_BA(:,1);
+    RCP85 = readtable('./DATA/RCP85_peat_simulations_results_v1.csv'); % Projections RCP85 only peatlands
     TREND_BA = table2array(RCP85(:,1:2));
-    ba_rcp85 = TREND_BA(:,1)+0.11*TREND_BA(:,1);
+    ba_rcp85peat = TREND_BA(:,1)+0.11*TREND_BA(:,1);
 
-    baClass_rcp45 = ones(length(ba_rcp45),1)*2.5;
-    baClass_rcp45(ba_rcp45<th1) = 2;
-    baClass_rcp45(ba_rcp45>th2) = 3;
-
-    baClass_rcp85 = ones(length(ba_rcp45),1)*2.5;
-    baClass_rcp85(ba_rcp85<th1) = 2;
-    baClass_rcp85(ba_rcp85>th2) = 3;
     
-    plot(1950:2100,baClass_rcp45-0.04,'.b')
-    plot(1950:2100,baClass_rcp85+0.04,'.r')
-    
-    
-    ylim([-0.4 3.4])
-    
-    set(gca,'XLim',[1980 2100],'XTick',1980:10:2100)
-    set(gca,'XTickLabelRotation',45)
-    
+subplot(4,4,[3 4]), hold on
+    bar([ba_rcp85peat ba_rcp85all-ba_rcp85peat],'stacked')
+    ylim([0 12])
+        
     set(gca, 'YGrid', 'off', 'XGrid', 'on')
     ax = gca;
     ax.GridColor = [0.6, 0.6, 0.6]; 
@@ -106,10 +74,22 @@ subplot(2,2,2), hold on
     set(gca,'FontName','Arial','FontSize',10);
 
     
-subplot(2,2,3), hold on    
+subplot(4,4,[7 8]), hold on
+    bar([ba_rcp45peat ba_rcp45all-ba_rcp45peat],'stacked')
+    ylim([0 12])
+    
+    set(gca, 'YGrid', 'off', 'XGrid', 'on')
+    ax = gca;
+    ax.GridColor = [0.6, 0.6, 0.6]; 
+    box on
+    set(gca,'FontName','Arial','FontSize',10);
 
-    RCP85all = readtable('./DATA/RCP85_all_simulations_results_v1.csv');
-    RCP45all = readtable('./DATA/RCP45_all_simulations_results_v1.csv');
+
+    
+subplot(4,4,[9 10 13 14]), hold on    
+
+    RCP85all = readtable('./DATA/RCP85_all_simulations_results_v1.csv'); % Projections RCP85 all Siberian Arctic
+    RCP45all = readtable('./DATA/RCP45_all_simulations_results_v1.csv'); % Projections RCP45 all Siberian Arctic
     shadedErrorBar(1980:2100,RCP85all.CO2eq(31:end),RCP85all.CO2eq_std(31:end)*2,'lineProps','r');
     shadedErrorBar(1980:2100,RCP45all.CO2eq(31:end),RCP45all.CO2eq_std(31:end)*2,'lineProps','b');
 
@@ -127,13 +107,14 @@ subplot(2,2,3), hold on
     box on
     set(gca,'FontName','Arial','FontSize',10);
 
-subplot(2,2,4), hold on    
+    
+subplot(4,4,[11 12 15 16]), hold on      
 
     RCP85peat = readtable('./DATA/RCP85_peat_simulations_results_v1.csv');
     RCP45peat = readtable('./DATA/RCP45_peat_simulations_results_v1.csv');
     shadedErrorBar(1980:2100,RCP85peat.CO2eq(31:end),RCP85peat.CO2eq_std(31:end)*2,'lineProps','r');
     shadedErrorBar(1980:2100,RCP45peat.CO2eq(31:end),RCP45peat.CO2eq_std(31:end)*2,'lineProps','b');
-    ylabel('CO2-eq PEAT (Tg)')
+%     ylabel('CO2-eq PEAT (Tg)')
     
     xlim([2000 2100])
     set(gca,'XLim',[1980 2100],'XTick',1980:10:2100)
@@ -151,19 +132,20 @@ subplot(2,2,4), hold on
 
 
 %% save svg
+% set(gcf, 'PaperPositionMode', 'auto')
+% saveas(gcf,['./figures/projections.png'])
+% saveas(gcf,['./figures/projections.svg'])
 
-set(gcf, 'PaperPositionMode', 'auto')
-saveas(gcf,['./figures/projections.png'])
-saveas(gcf,['./figures/projections.svg'])
 
-
-%% ratio all peat
-
-% figure, plot(RCP85all.CO2eq,RCP85peat.CO2eq,'.')
+%% Statistics
 display(['RATIO direct C EMISSIONS: ' num2str(nanmean(RCP85peat.C./RCP85all.C)) '% +-' num2str(nanstd(RCP85peat.C./RCP85all.C)) '%'])
 
-
-%% stats paper
+RCP45 = readtable('./DATA/RCP45_all_simulations_results_v1.csv');
+TREND_BA = table2array(RCP45(:,1:2));
+ba_rcp45 = TREND_BA(:,1)+0.11*TREND_BA(:,1);
+RCP85 = readtable('./DATA/RCP85_all_simulations_results_v1.csv');
+TREND_BA = table2array(RCP85(:,1:2));
+ba_rcp85 = TREND_BA(:,1)+0.11*TREND_BA(:,1);
 
 years = 1950:2100;
 % figure, plot(years, ba_rcp45')
