7a49c949b2832ad808e1ed58c41627aa75f6c6d5
Adrià Descals <50245567+adriadescals@users.noreply.github.com>
2022-11-03 15:05:53 +0100

    Update e03_extract_CAPEhourly.m

--- main/e03_extract_CAPEhourly.m
+++ main/e03_extract_CAPEhourly.m
@@ -1,56 +1,60 @@
+%% Extracts ERA5-Land CAPE hourly and make statistics.
+% CAPE hourly data (.nc) is not provided in the GitHub repository. The data can be
+% downloaded at https://doi.org/10.24381/cds.e2161bac
+
 clear all; close all;
 
+%% Define region of interest
 roi = imread('./DATA/roi_arctic_CAPE.tif');
 roi = [zeros(50,441); roi(:,1:441); zeros(5,441)];
 roi = flip(rot90(roi));
 
-%% SAVE sample CAPE
-pathFileCAPE = 'CAPE_2011_2020.nc'
+%% SAVE sample CAPE as image
+if 0
+    pathFileCAPE = '../large_datasets/CAPEhourly/CAPE_2011_2020.nc';
 
-finfo = ncinfo(pathFileCAPE);
-ncdisp(pathFileCAPE)
+    finfo = ncinfo(pathFileCAPE);
+    ncdisp(pathFileCAPE)
 
-lat = double(ncread(pathFileCAPE,'latitude'));
-lon = double(ncread(pathFileCAPE,'longitude'));
-time = ncread(pathFileCAPE,'time');
+    lat = double(ncread(pathFileCAPE,'latitude'));
+    lon = double(ncread(pathFileCAPE,'longitude'));
+    time = ncread(pathFileCAPE,'time');
 
-for ii = 9
+    for ii = 9
 
-CAPE = ncread(pathFileCAPE,'cape',[1 1 1+2952*ii],[441 101 2952]);
+    CAPE = ncread(pathFileCAPE,'cape',[1 1 1+2952*ii],[441 101 2952]);
 
-CAPEf =flipud(rot90(mean(CAPE,3)));
-figure, imagesc(CAPEf)
+    CAPEf =flipud(rot90(mean(CAPE,3)));
+    figure, imagesc(CAPEf)
 
-title(ii+2011)
+    title(ii+2011)
 
-% pause
-% close all
+    % pause
+    % close all
+    end
+
+    CAPE = ncread(pathFileCAPE,'cape',[1 1 27998],[441 101 1]);
+
+    [rows cols bands] = size(CAPE);
+    R = georasterref('RasterSize', [rows cols bands], ...
+        'RasterInterpretation', 'cells', 'ColumnsStartFrom', 'north', 'RowsStartFrom', 'west', ...
+        'LatitudeLimits', [min(lat) max(lat)], 'LongitudeLimits', [min(lon) max(lon)]);
+
+    geotiffwrite('./DATA/CAPE_sample',CAPE,R)
 end
 
-CAPE = ncread(pathFileCAPE,'cape',[1 1 27998],[441 101 1]);
-
-[rows cols bands] = size(CAPE);
-R = georasterref('RasterSize', [rows cols bands], ...
-    'RasterInterpretation', 'cells', 'ColumnsStartFrom', 'north', 'RowsStartFrom', 'west', ...
-    'LatitudeLimits', [min(lat) max(lat)], 'LongitudeLimits', [min(lon) max(lon)]);
-
-geotiffwrite('./figures/CAPE_ind27998',CAPE,R)
-
-%% SAVE hourly estimates Siberia
+%% SAVE hourly estimates averaged over the Siberian Arctic
 
 PERIODS = {'2001_2010','2011_2020'};
 roi3d = repmat(roi==1,1,1,2952);
 
 meanCAPE = [];
-meanRF = [];
 
 for iperiod = 1:2
 
-    pathFileRF = ['RF_' PERIODS{iperiod} '.nc'];
-    pathFileCAPE = ['CAPE_' PERIODS{iperiod} '.nc'];
+    pathFileCAPE = ['../large_datasets/CAPEhourly/CAPE_' PERIODS{iperiod} '.nc'];
 
     meanCAPEii = nan(2952,10);
-    meanRFii = nan(2952,10);
 
     yy = 1;
     h = waitbar(0,'Please wait...');
@@ -61,9 +65,6 @@ for iperiod = 1:2
 
         meanCAPEii(:,yy)= squeeze(nanmean(nanmean(CAPE,1),2));
 
-        RF = ncread(pathFileRF,'tp',[1 1 itime],[441 101 2952]);
-        meanRFii(:,yy)= squeeze(nanmean(nanmean(RF,1),2));
-
         yy = yy+1;
         h = waitbar(itime/29520,h)
         yy
@@ -71,20 +72,12 @@ for iperiod = 1:2
     close(h)
 
     meanCAPE = [meanCAPE meanCAPEii];
-    meanRF = [meanRF meanRFii];
 
 end
+
 doyCAPE = 121:243;
 
-save('./DATA/Arctic_CAPE_RF_hourly_2001-2020','meanCAPE','meanRF','doyCAPE')
+save('./DATA/Arctic_CAPE_hourly_2001-2020','meanCAPE','doyCAPE')
 
 
-CAPERFdiv = meanCAPE./meanRF;
-CAPERFprod = meanCAPE.*meanRF;
 
-figure, plot(meanCAPE(:),'.')
-figure, plot(meanCAPE,'.')
-
-figure, plot(2001:2020,mean(meanCAPE,1),'o')
-figure, plot(2001:2020,mean(CAPERFprod,1),'o')
-figure, plot(2001:2020,mean(CAPERFdiv,1),'o')
